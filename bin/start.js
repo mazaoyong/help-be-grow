fd1cc3ebb191273a083402e8aced0fa755b6ff77
mazaoyong <mazaoyong@youzan.com>
2021-10-08 10:08:18 +0800

    fix: 尝试自动打包

--- bin/start.js
+++ bin/start.js
@@ -1,2 +1,7 @@
-#! /usr/bin/env node
-require('../app/index.js')
+const child_process = require('child_process');
+child_process.exec('npm run build', err => {
+  if (err) {
+    console.log('打包报错：', err)
+  }
+  require('../app/index.js')
+})
