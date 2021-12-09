33d35b9b5f8f98a263bdcc95a2c8560d4e83c2bd
zhoutt <zhoutingting@youzan.com>
2021-12-09 20:50:17 +0800

    feat: 组件影响面 - 补充了提醒的详细程度 and code style and 埋点

---
+++ client/src/pages/findPage/enum.tsx
@@ -0,0 +1,5 @@
+export const LIB_DOC_URL: any = {
+    '@youzan/vis-ui': 'https://fedoc.qima-inc.com/vis-ui#/',
+    '@youzan/ebiz-components': 'https://fedoc.qima-inc.com/ebiz-react-components/guidebook',
+    '@youzan/react-components': 'https://fedoc.qima-inc.com/react-components/#/',
+};
